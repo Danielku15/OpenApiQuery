4896e7d9c00f17b82cb5580374f35f682fb8171c
Daniel Kuschny <danielku15@coderline.net>
2019-11-15 17:32:37 +0100

    Added test project

---
+++ OpenApiQuery.Test/ExampleTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenApiQuery.Test
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod]
+        public void InitialTest()
+        {
+            Assert.IsTrue(true, "Test works");
+        }
+    }
+}
